faa1a75bf5c7989a34dcd80763df4f9fd9450f0c
Carlos Loaiza <carlos.loaiza@auplex.mx>
2023-03-24 11:52:00 -0700

    se agregó routers y pagina

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+// import { createRouter, createWebHistory } from "vue-router";
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+// });
+
+// import LandingPage from "../views/LandingPage.vue";
+
+// const routes = [
+// //   {
+// //     path: "/",
+// //     redirect: "/LandingPage",
+// //   },
+
+//   {
+//     path: '/LandingPage',
+//     name: 'LandingPage',
+//     component: LandingPage,
+//     meta: {
+//       isPublic: true
+//     }
+//   },
+// ];
+
+
+// export default router
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hola</div>
+  <section>ohlla jaf ia</section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
